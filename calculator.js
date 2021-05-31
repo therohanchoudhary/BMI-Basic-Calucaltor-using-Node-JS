ed0e98e0d795e5bd5725c91e1ef8a209a82886b6
therohanchoudhary <41481277+therohanchoudhary@users.noreply.github.com>
2021-05-31 08:47:16 +0530

    Minor Changes

--- calculator.js
+++ calculator.js
@@ -5,11 +5,11 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/bmicalculator",function(req,res){
-  res.sendFile(__dirname + "/bmiCalculator.html");
+app.get("/",function(req,res){
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.post("/bmicalculator",function(req,res){
+app.post("/",function(req,res){
   var height = parseFloat(req.body.height);
   var weight = parseFloat(req.body.weight);
   var result = height/(weight*weight);
